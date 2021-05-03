04a5fc841defd8916cc2427b79a2d34058b3c1aa
yunusemreguler <yeguler81@gmail.com>
2021-05-03 11:56:47 +0300

    Created a Spring application with Spring initializr.

---
+++ src/main/java/com/example/bookifyservice/BookifyserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.bookifyservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookifyserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookifyserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bookifyservice/BookifyserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookifyservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookifyserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
