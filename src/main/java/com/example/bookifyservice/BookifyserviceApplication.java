4bb0dd37e3ab45aa8a213e8890c7abd6cbba03b8
yunusemreguler <yeguler81@gmail.com>
2021-05-06 23:24:42 +0300

    Added swagger.

--- src/main/java/com/example/bookifyservice/BookifyserviceApplication.java
+++ src/main/java/com/example/bookifyservice/BookifyserviceApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@EnableMongoRepositories
 public class BookifyserviceApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/example/bookifyservice/controller/BookController.java
+++ src/main/java/com/example/bookifyservice/controller/BookController.java
@@ -11,8 +11,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.Instant;
-
 @RestController
 @RequestMapping("/api/1.0/book")
 @AllArgsConstructor
